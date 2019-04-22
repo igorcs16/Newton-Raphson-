d65b34cac67ffbb0197219aa83ba6d7a81f8556c
Igor Castro Santilli <igor.santilli@unisys.com>
2019-04-22 11:19:46 -0300

    Funcoes e variaveis exigidas para o projeto

--- newton.c
+++ newton.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+double deposito[10];
+int tempo[10];
+double saldo;
+int t_s;
+
+static double newton(double epsilon);
+
+int main()
+{
+}
+
+static double newton(double epsilon)
+{
+
+}
