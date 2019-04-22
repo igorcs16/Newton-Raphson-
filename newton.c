cadc939622e2d2283ceb91c2871a1affa131b694
Igor Castro Santilli <igor.santilli@unisys.com>
2019-04-22 16:41:15 -0300

    Armazena valor de deposito e o mes correspondente

--- newton.c
+++ newton.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <math.h>
 
+#define TRUE   1
+#define FALSE  0
+
 double deposito[10];
 int tempo[10];
 double saldo;
@@ -12,9 +15,25 @@ static double newton(double epsilon);
 
 int main()
 {
+  double e = 0.5;
+
+  newton(e);
+
+  return 0;
 }
 
 static double newton(double epsilon)
 {
+  int i = 0;
 
+  printf("Digite os valores dos depositos e o mes de cada deposito\n");
+
+  for(i = 0; i < 5; i++)
+  {
+    printf("Valor:\n");
+    scanf(" %lf", &deposito[i]);
+
+    printf("Mes:\n");
+    scanf(" %d", &tempo[i]);
+  }
 }
