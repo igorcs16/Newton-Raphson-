f69060fa5394a31978679d20859eeafadc1031a8
igorcs16 <igor.ca.santilli@gmail.com>
2019-04-22 22:42:05 -0300

    Calculo de funcao e derivada para taxa de juros compostos

--- newton.c
+++ newton.c
@@ -15,7 +15,28 @@ static double newton(double epsilon);
 
 int main()
 {
-  double e = 0.5;
+  double e;
+  int i = 0;
+  
+  printf("Digite os valores dos depositos e o mes de cada deposito\n");
+
+  for(i = 0; i < 5; i++)
+  {
+    printf("Valor:");
+    scanf(" %lf", &deposito[i]);
+
+    printf("Mes:");
+    scanf(" %d", &tempo[i]);
+  }
+
+  printf("\nSaldo:\n");
+  scanf(" %lf", &saldo);
+
+  printf("Mes Saldo:\n");
+  scanf(" %d", &t_s);
+  
+  printf("Valor de Precisao:");
+  scanf(" %lf", &e);
 
   newton(e);
 
@@ -24,16 +45,54 @@ int main()
 
 static double newton(double epsilon)
 {
-  int i = 0;
+  double jk = 0.5;
+  double jkn[10];
+  double F = 0.0;
+  double f;
 
-  printf("Digite os valores dos depositos e o mes de cada deposito\n");
+  double modulo;
 
-  for(i = 0; i < 5; i++)
+  int cont, i;
+
+  if(epsilon >= 0 && epsilon <= 1)
   {
-    printf("Valor:\n");
-    scanf(" %lf", &deposito[i]);
+    for(i = 0; i < sizeof(deposito); i++)
+    {
+      //funcao
+      if(i == 0)
+      {
+        for(cont = 0; cont < sizeof(deposito); cont++)
+        {
+          F = deposito[cont]*pow(1 + jk, tempo[cont]) + F;
+          f = tempo[cont]*deposito[cont]*pow(1 + jk, tempo[cont]-1) + f;
+        }
 
-    printf("Mes:\n");
-    scanf(" %d", &tempo[i]);
+        jkn[i] = jk - (F/f);
+
+        modulo = (jkn[i] - jk) * (-1);
+
+        if(modulo < epsilon)
+          return jk;
+      }
+
+      else
+      {
+        for(cont = 0; cont < sizeof(deposito); cont++)
+        {
+          F = deposito[cont]*pow(1 + jkn[i], tempo[cont]) + F;
+          f = tempo[cont]*deposito[cont]*pow(1 + jkn[i], tempo[cont]-1) + f;
+        }
+
+        jkn[i+1] = jk - (F/f);
+
+        modulo = (jkn[i + 1] - jkn[i]) * (-1);
+
+        if(modulo < epsilon)
+          return jkn[i];
+      }
+
+    }
   }
+
+  return -1; 
 }
