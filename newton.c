c9d8afd4acb11cfcb0e2d767d68f129b15636972
igorcs16 <igor.ca.santilli@gmail.com>
2019-05-18 17:50:52 -0300

    .

--- newton.c
+++ newton.c
@@ -1,93 +1,90 @@
-#include <stdio.h> // Biblioteca para funções de entrada e saída de dados
-#include <stdlib.h> // Biblioteca para utilidades gerais
-#include <math.h> // Biblioteca para funções matemáticas
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-/*
- * Grupo:
- * ALAN DIAS CHAGAS            RA: 20870938
- * ANA PAULA DOS SANTOS SOUZA  RA: 20894639
- * IGOR CASTRO SANTILLI        RA: 20909361
- */
+#define TAM 10
 
+double deposito[10]; 
+int tempo[10];
+double saldo;
+int t_s;
 
-#define TAM 5 // Tamanho total para percorrer todo um vetor em uma estrutura de repetição
+static double newton(double epsilon);
 
-double deposito[10]; // declaração de variável global para armazenar depósitos 
-int tempo[10]; // declaração de variável global para armazenar os meses dos depósitos
-double saldo; // declaração de variável global para armazenar saldo final
-int t_s; // declaração de variável global para armazenar mês final
-
-static double newton(double epsilon); // Protótipo da função newton
-
-int main() // função principal
+int main()
 {
-  double e = 0.0; // declaração do valor de precisão
-  int i = 0; // declaração do contador do laço for
-  char perc = '%'; // símbolo de porcentagem
+  double e = 0.0;
+  int i = 0;
+  char perc = '%';
+  double juros = 0.0;
    
-  printf("Digite os valores dos depósitos e o mês de cada depósito\n"); // Imprime pedido para entrada de dados
+  printf("Digite os valores dos depósitos e o mês de cada depósito\n");
 
-  for(i = 0; i < TAM; i++) // estrutura de repetição para armazenar valores de acordo com o Tamanho definido (TAM)
+  for(i = 0; i < TAM; i++)
   {
-    printf("Valor:"); // Imprime "Valor" para o usuario digitar o valor de depósito 
-    scanf(" %lf", &deposito[i]); // Armazena valor de depósito na variável deposito[i] informado
+    printf("Valor:"); 
+    scanf(" %lf", &deposito[i]);
 
-    printf("Mês:"); // Imprime "Mês" para o usuario passar o mês do depósito correspondente
-    scanf(" %d", &tempo[i]); // Armazena na variável tempo[i] mês correspondente ao depósito informado
+    printf("Mês:");
+    scanf(" %d", &tempo[i]);
 
-    printf("\n"); // Quebra de linha
+    printf("\n");
   }
 
-  printf("Saldo:"); // Imprime "Saldo" para o usuario digitar o saldo final
-  scanf(" %lf", &saldo); // Armazena saldo final na variável saldo
+  printf("Saldo:");
+  scanf(" %lf", &saldo);
 
-  printf("Mês Saldo:"); // Imprime "Mês Saldo" para o usuario digitar o mês do saldo correspondente 
-  scanf(" %d", &t_s); // Armazena na variável t_s o mês informado 
+  printf("Mês Saldo:"); 
+  scanf(" %d", &t_s); 
   
-  printf("\nValor de Precisão:"); // Imprime "Valor de Precisão" para o usuário
-  scanf(" %lf", &e); // Armazena valor de precisão informado na variável e
+  printf("\nValor de Precisão:");
+  scanf(" %lf", &e);
 
-  // Imprime na tela a taxa de juros anual, chamando a função newton e passando como parâmetro o valor de precisão
-  printf("\nTaxa de juros anual é de aproximadamente: %.2lf%c\n", newton(e), perc);
+  juros = newton(e);
+  
+  if(juros == -1) 
+    printf("\n%.0lf\nErro, valor de precisão inválido\n", juros);
+  else
+      printf("\nTaxa de juros anual é de aproximadamente: %.2lf%c\n", juros, perc);
 
-  return 0; // FIM do programa, retorno da função main
+  return 0;
 }
 
-static double newton(double epsilon) // "Corpo" da função newton 
+static double newton(double epsilon) 
 {
-  double jkn = 0.5; // declaração de variável local, jkn = 0.5 é um valor de aproximação inicial de juros
-  double aux = 0.0; // declaração de variável local auxiliar
-  double F = 0.0; // declaração de variável local, função f(x)
-  double f = 0.0; // declaração de variável local, função derivada f'(x)
+  double jkn = 0.5;
+  double aux = 0.0;
+  double F = 0.0;
+  double f = 0.0;
 
-  double modulo = 0.0; // declaração de variável local
+  double modulo = 0.0;
 
-  int cont = 0; // declaração de variável local, contador. 
+  int cont = 0; 
 
-  if(epsilon >= 0 && epsilon <= 1) // estrutura de conditional if, verifica se valor de precisão é válido  
+  if(epsilon >= 0 && epsilon <= 1)  
   {
-    do // inicio da estrutura de repetição "do while"
+    do
     {
-      F = f = 0.0; // inicializa as funções para calcular o novo valor de juros 
+      F = f = 0.0; 
 
-      for(cont = 0; cont < TAM; cont++) // estrutura de repetição for, para cálculo de depósitos e meses armazenados 
+      for(cont = 0; cont < TAM; cont++) 
       {
-        F += deposito[cont]*pow(1 + jkn, t_s - tempo[cont]); // cálculo da função f(jkn)
-        f += (t_s - tempo[cont])*deposito[cont]*pow(1 + jkn, t_s - tempo[cont] -1); // cálculo da função derivada f'(jkn)
+        F += deposito[cont]*pow(1 + jkn, t_s - tempo[cont]);
+        f += (t_s - tempo[cont])*deposito[cont]*pow(1 + jkn, t_s - tempo[cont] -1);
       }
       
-      F -= saldo; // subtrai da função o valor do saldo final
+      F -= saldo;
 
-      aux = jkn; // armazena o valor de jkn em uma variável auxiliar, para calcular o próximo valor de precisão de juros
-      jkn = jkn - (F/f); // cálculo do valor de juros 
+      aux = jkn;
+      jkn = jkn - (F/f); 
 
-      modulo = (jkn - aux) * (jkn < aux ? (-1) : 1); // módulo de jkn(próximo) - jkn(corrente) 
+      modulo = (jkn - aux) * (jkn < aux ? (-1) : 1); 
 
-    } while(modulo >= epsilon); // reinicia o looping caso o modulo seja maior ou igual a epsilon(valor de precisao passado como parâmetro para função
+    } while(modulo >= epsilon);
 
-    return jkn*100; // retorna o valor de juros com base no valor de precisão
+    return jkn*100;
 
   }
 
-  return -1; // retorna -1 caso o valor de precisão não seja válido(epsilon >= 0 && epsilon <= 1)
+  return -1;
 }
